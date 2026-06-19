632c701b2c2ee26d91f0f20f38018e741f9eb6d9
SudevOP1 <sudevdahitule06@gmail.com>
2026-06-19 14:55:34 +0530

    cleared codebase to start fresh

--- apps/backend/backend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    path("base/", include("base.urls")),
-    path("auth/", include("users.urls"))
-]
--- apps/frontend/src/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { useAuthContext } from "./context/AuthContext.jsx";
-
-const PrivateRoute = ({ children }) => {
-  const { loggedIn } = useAuthContext();
-  return loggedIn ? children : <Navigate to="/" />;
-};
-
-export default PrivateRoute;
